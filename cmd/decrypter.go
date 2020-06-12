91c6cd3bbd7387f81e0df976b3502f910c207d3f
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-06-12 02:25:47 -0300

    Adding spinner!

--- cmd/decrypter.go
+++ cmd/decrypter.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/briandowns/spinner"
 )
+
+var s = spinner.New(spinner.CharSets[14], 100*time.Millisecond)
 
 func StartDecryption(wordlist_path, informed_hash string, encryption_func func(*string) string) string {
 	start := time.Now()
@@ -15,13 +19,16 @@ func StartDecryption(wordlist_path, informed_hash string, encryption_func func(*
 
 	scanner := bufio.NewScanner(f)
 
+	s.Start()
 	for scanner.Scan() {
 		current_word := scanner.Text()
 		encryptedWord := encryption_func(&current_word)
 		if encryptedWord == informed_hash {
+			s.Stop()
 			return fmt.Sprintf("Decrypted hash! Value: %s - Took: %x milliseconds\n", current_word, time.Since(start).Milliseconds())
 		}
 	}
+	s.Stop()
 	return fmt.Sprintf("Not found. - Took: %x milliseconds\n", time.Since(start).Milliseconds())
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mdcg/hashkiller
 
 go 1.14
+
+require github.com/briandowns/spinner v1.6.1
