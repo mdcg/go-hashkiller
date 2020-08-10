4a16ffa99362d8e5af02772cc0c8bfddc5b2c47c
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-08-10 02:26:15 -0300

    typo

--- main.go
+++ main.go
@@ -12,5 +12,4 @@ func main() {
 	encryption_func := cmd.ChooseEncryptionType(*encryption_type)
 	result := cmd.StartDecryption(*wordlist_path, *informed_hash, encryption_func)
 	fmt.Printf(result)
-	// 2ee2ef1dbc555ce6a84533475715806c
 }
